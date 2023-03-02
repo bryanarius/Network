329529f24f8265799d527f6cf249b7389906f36a
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-03-01 20:46:25 -0500

    Added pagination to all posts page

--- network/views.py
+++ network/views.py
@@ -3,16 +3,25 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
 
 from .models import User, Post
 
 
 def index(request):
     allPosts = Post.objects.all().order_by("id").reverse()
+
+    # Pagination
+    paginator = Paginator(allPosts, 1)
+    page_number = request.GET.get('page')
+    posts_of_the_page = paginator.get_page(page_number)
+
     return render(request, "network/index.html", {
-        "allPosts": allPosts
+        "allPosts": allPosts,
+        "posts_of_the_page": posts_of_the_page
     })
 
+    
 def newPost(request):
     if request.method ==  "POST":
         content = request.POST['content']
